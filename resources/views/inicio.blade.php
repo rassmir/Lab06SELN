ce7c8a2a91124d5a63b2af08bbfb1722bdbc097f
Rassmir Flores <rassmir.flores@tecsup.edu.pe>
2019-09-09 04:08:29 -0500

    nuevos cambios

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Panamericanos-2019</title>
+    <link href="{{ asset('css/css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/css/mdb.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/css/accordion-extended.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/css/megamenu.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/css/animations-extended.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/css/lightbox.css') }}" rel="stylesheet">
+
+</head>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+    <div class="container-fluid">
+
+        <!-- Brand -->
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('img/logo.svg') }}" height="65" alt="panamericanos logo">
+        </a>
+
+        <!-- Collapse -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+    </div>
+</nav>
+
+<div class="view full-page-intro" style="background-image: url('{{ asset('img/slider3.png') }}'); background-repeat: no-repeat; background-size: cover;">
+    <!-- Content -->
+    <div class="container" style="margin-top: 110px" >
+        <div class="row wow fadeIn">
+            <!--Grid column-->
+            <div class="col-md-6 col-xl-5">
+                <!--Card-->
+                <div class="card pt-3 ">
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs md-tabs nav-justified default-color" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#panel555" role="tab">
+                                <i class="fas fa-user pr-2"></i>Iniciar Sesión</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#panel666" role="tab">
+                                <i class="fas fa-clipboard-list pr-2"></i></i>Registrarse</a>
+                        </li>
+                    </ul>
+                    <!-- Nav tabs -->
+
+                    <!-- Tab panels -->
+                    <div class="tab-content">
+
+                        <!-- Panel 1 -->
+                        <div class="tab-pane fade in show active" id="panel555" role="tabpanel">
+
+                            <!-- Form -->
+                            <form class=" border p-4 border-default" action="{{ route('login') }}" method="post">
+                                @csrf
+                                <div class="md-form mb-3">
+                                    <i class="fas fa-user prefix grey-text"></i>
+                                    <input type="email" id="email" class="form-control validate" name="email">
+                                    <label data-error="wrong" data-success="right" for="email">Correo Electrónico</label>
+                                </div>
+                                <div class="md-form mb-3">
+                                    <i class="fas fa-envelope prefix grey-text"></i>
+                                    <input type="password" id="password" class="form-control validate" name="password">
+                                    <label data-error="wrong" data-success="right" for="password">Contraseña</label>
+                                </div>
+
+                                <!-- Sign in button -->
+                                <button class="btn btn-info btn-block" type="submit">Ingresar</button>
+
+                            </form>
+                            <!-- Form -->
+
+                        </div>
+                        <!-- Panel 1 -->
+
+                        <!-- Panel 2 -->
+                        <div class="tab-pane fade" id="panel666" role="tabpanel">
+
+                            <!-- Form -->
+                            <form class=" border p-4 border-default" action="{{ route('register') }}" method="post">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="md-form mb-3">
+                                            <input type="text" id="nombre" class="form-control validate" name="name">
+                                            <label data-error="wrong" data-success="right" for="nombre">Nombre</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="md-form mb-3">
+                                            <input type="text" id="apellidos" class="form-control validate" name="lastname">
+                                            <label data-error="wrong" data-success="right" for="apellidos">Apellidos</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="md-form mb-3">
+                                    <input type="email" id="correo" class="form-control validate" name="email">
+                                    <label data-error="wrong" data-success="right" for="correo">Email</label>
+                                </div>
+
+                                <div class="md-form mb-3">
+                                    <input type="password" id="clave" class="form-control validate" name="password">
+                                    <label data-error="wrong" data-success="right" for="clave">Password</label>
+                                </div>
+                                <div class="md-form mb-3">
+                                    <input type="password" id="confirmation" class="form-control validate" name="password_confirmation">
+                                    <label data-error="wrong" data-success="right" for="confirmation">Password Confirmation</label>
+                                </div>
+                                <!-- Sign in button -->
+                                <button class="btn btn-info btn-block" type="submit">Ingresar</button>
+
+
+                            </form>
+
+                        </div>
+                        <!-- Panel 2 -->
+
+                    </div>
+                    <!-- Tab panels -->
+
+
+                </div>
+                <!--/.Card-->
+
+            </div>
+
+            <!--Grid column-->
+            <div class="col-md-6 mb-4 white-text text-center text-md-left">
+
+                <h1 class="display-4 font-weight-bold">No te olvides ... !</h1>
+                <hr class="hr-light ">
+                <p>
+                    <strong>Iniciar Sesión para ver el listado de deportes</strong>
+                </p>
+            </div>
+        </div>
+        <!--Grid row-->
+
+    </div>
+    <!-- Content -->
+
+</div>
+
+
+<!-- Scripts -->
+<script src="{{ asset('js/js/jquery-3.3.1.min.js') }}" ></script>
+<script src="{{ asset('js/js/bootstrap.js') }}" ></script>
+<script src="{{ asset('js/js/mdb.js') }}" ></script>
+<script src="{{ asset('js/js/dropdown.js') }}" ></script>
+<script src="{{ asset('js/js/popper.min.js') }}" ></script>
+<script src="{{ asset('js/js/sidenav.js') }}" ></script>
+<!-- Fonts -->
+<script src="https://kit.fontawesome.com/0277df775c.js"></script>
+</body>
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,105 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Panamericanos-2019</title>
+    <link href="{{ asset('css/admin/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin/mdb.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin/accordion-extended.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin/megamenu.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin/animations-extended.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin/lightbox.css') }}" rel="stylesheet">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+<body class="dark-edition">
+<div class="wrapper ">
+    <div class="sidebar" data-color="purple" data-background-color="black" data-image="./assets/img/sidebar-2.jpg">
+        <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+        Tip 2: you can also add an image using data-image tag
+    -->
+        <div class="logo">
+            <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+                Creative Tim
+            </a>
+        </div>
+        <div class="sidebar-wrapper">
+            <ul class="nav">
+                <li class="nav-item active  ">
+                    <a class="nav-link" href="javascript:void(0)">
+                        <i class="material-icons">dashboard</i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <!-- your sidebar here -->
+            </ul>
+        </div>
+    </div>
+    <div class="main-panel">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+            <div class="container-fluid">
+                <div class="navbar-wrapper">
+                    <a class="navbar-brand" href="javascript:void(0)">Dashboard</a>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
+                    <span class="navbar-toggler-icon icon-bar"></span>
                 </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                <div class="collapse navbar-collapse justify-content-end">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="javascript:void(0)">
+                                <i class="material-icons">notifications</i>
+                                <p class="d-lg-none d-md-block">
+                                    Notifications
+                                </p>
+                            </a>
+                        </li>
+                        <!-- your navbar here -->
                     </ul>
                 </div>
             </div>
         </nav>
+        <!-- End Navbar -->
+        <div class="content">
+            <div class="container-fluid">
+                <!-- your content here -->
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="container-fluid">
+                <nav class="float-left">
+                    <ul>
+                        <li>
+                            <a href="https://www.creative-tim.com">
+                                Creative Tim
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+
+            </div>
+        </footer>
     </div>
+</div>
+
+    <script src="{{ asset('js/admin/jquery.min.js') }}" ></script>
+    <script src="{{ asset('js/admin/popper.min.js') }}" ></script>
+    <script src="{{ asset('js/admin/bootstrap-material-design.min.js') }}" ></script>
+    <script src="https://unpkg.com/default-passive-events"></script>
+    <script src="{{ asset('js/admin/perfect-scrollbar.jquery.min.js') }}" ></script>
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <script src="{{ asset('js/admin/material-dashboard.js?v=2.1.0') }}" ></script>
+    <script src="{{ asset('js/admin/custom.js') }}" ></script>
+    <!-- Fonts -->
+    <script src="https://kit.fontawesome.com/0277df775c.js"></script>
 </body>
 </html>
