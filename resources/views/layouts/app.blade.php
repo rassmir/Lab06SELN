dc7c4e7b760a5baca851002987723beb6b76fba4
Rassmir Flores <rassmir.flores@tecsup.edu.pe>
2019-09-09 05:04:02 -0500

    nuevos cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Panamericanos-2019</title>
-    <link href="{{ asset('css/admin/material-dashboard.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/admin/material-dashboard.css') }}" rel="stylesheet">
 
 </head>
 <body class="dark-edition">
